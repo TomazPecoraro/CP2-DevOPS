9c8ade4a00fa6930eaaf626b74e4b7791890de60
tomazpecoraro <tomazpecoraro0@gmail.com>
2024-10-14 22:12:23 -0300

    Correcoes

--- Sprint3-.NET/Sprint3.API/Program.cs
+++ Sprint3-.NET/Sprint3.API/Program.cs
@@ -1,40 +1,49 @@
 using Sprint3.API.Configuration;
 using Sprint3.API.Extensions;
+using Microsoft.OpenApi.Models;
 using Microsoft.Extensions.Configuration;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+{
+    ContentRootPath = AppContext.BaseDirectory,
+    Args = args,
+});
 
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    serverOptions.ListenAnyIP(5000); // Porta HTTP
+    serverOptions.ListenAnyIP(5001, listenOptions => listenOptions.UseHttps()); // Porta HTTPS
+});
 
 IConfiguration configuration = builder.Configuration;
 AppConfiguration appConfiguration = new AppConfiguration();
 configuration.Bind(appConfiguration);
 builder.Services.Configure<AppConfiguration>(configuration);
 
-// Add services to the container.
-
+// Adicionando serviços ao contêiner
 builder.Services.AddControllers();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+});
 
 builder.Services.AddDBContexts(appConfiguration);
-builder.Services.AddSwagger(appConfiguration);
 builder.Services.AddServices();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => 
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = string.Empty; // Mapeia para a raiz
+    });
 }
 
-app.UseHttpsRedirection();
-
+//app.UseHttpsRedirection();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
